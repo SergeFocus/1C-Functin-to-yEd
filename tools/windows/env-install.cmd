b4896d245a64fb1a94454497893b42419804f967
Alexey Lustin <alustin@silverbulleters.org>
2016-02-02 01:39:18 +0300

    для choko достаточно вызова команды Install - апгрейд пройдет автоматически

--- tools/windows/env-install.cmd
+++ tools/windows/env-install.cmd
@@ -4,11 +4,14 @@ cls
 @choco install notepadplusplus -y
 echo Waiting for aproved https://chocolatey.org/packages/SourceTree, install skiped
 echo choco install sourcetree
-@choco install python
-@choco install wget
-@choco install pickles
-@choco install picklesui
-@choco install pandoc
-@choco install irfanview
+@choco install python -y
+@choco install wget -y
+@choco install pickles -y
+@choco install picklesui -y
+@choco install pandoc -y
+@choco install irfanview -y
 echo Wainting for MSI for oscript.io
 echo choco install oscript
+@choco install sublimetext3 -y
+@choco install atom -y
+@choco install plantuml -y

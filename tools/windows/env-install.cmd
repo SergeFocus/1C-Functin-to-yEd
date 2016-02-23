802117d6525dca2efce4206a715099daa287f4f1
Alexey Lustin <alustin@silverbulleters.org>
2016-02-23 04:57:41 +0300

    убран force и добавлен отдельный файл для обновления, чтобы не было конфликтов

--- tools/windows/env-install.cmd
+++ tools/windows/env-install.cmd
@@ -1,26 +1,26 @@
 @echo off
 cls
-@choco install conemu --force -y
-@choco install git --force -y -params '"/GitAndUnixToolsOnPath"'
-@choco install --force notepadplusplus -y
-echo Waiting for aproved https://chocolatey.org/packages/SourceTree, install skiped
-echo choco install sourcetree
-@choco install --force python -y
-@choco install --force wget -y
-@choco install --force pickles -y
-@choco install --force picklesui -y
-@choco install --force pandoc -y
-@choco install --force irfanview -y
+@choco install conemu -y
+@choco install git -y -params '"/GitAndUnixToolsOnPath"'
+@choco install notepadplusplus -y
+@choco install sourcetree
+@choco install python -y
+@choco install ruby -y
+@choco install wget -y
+@choco install pickles -y
+@choco install picklesui -y
+@choco install pandoc -y
+@choco install irfanview -y
 echo Wainting for MSI for oscript.io
 echo choco install oscript
-@choco install --force sublimetext3 -y
-@choco install --force atom -y
-@choco install --force plantuml -y
+@choco install sublimetext3 -y
+@choco install atom -y
+@choco install plantuml -y
 
 echo addition cloud provider
 
-@choco install --force virtualbox -y
-@choco install --force vagrant -y
-@choco install --force docker-machine -y
-@choco install --force docker -y
-@choco install --force docker-compose -y
+@choco install virtualbox -y
+@choco install vagrant -y
+@choco install docker-machine -y
+@choco install docker -y
+@choco install docker-compose -y
